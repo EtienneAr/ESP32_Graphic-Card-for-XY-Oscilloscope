b779b8900e18faf0f05e40897f1fba1dcb350d9e
Me <etienne.arlaud@free.fr>
2020-09-26 16:50:42 +0200

    A real drawing

--- components/DAC_XY_Plotter/src/I2SManager.c
+++ components/DAC_XY_Plotter/src/I2SManager.c
@@ -9,6 +9,12 @@
 static const char* TAG = "I2S-Manager";
 
 #define I2S_PORT ((i2s_port_t) 0)
+#define I2S_BUF_LEN 1024
+#define I2S_BUF_COUNT 4
+
+#define CONVERSION_CHUNK_SIZE 1024
+
+#define min(x,y) ((x)<(y) ? (x) : (y))
 
 void I2SManager_init(int rate) {
 	ESP_LOGI(TAG, "init I2S");
@@ -20,8 +26,8 @@ void I2SManager_init(int rate) {
 	    .communication_format = I2S_COMM_FORMAT_I2S_MSB, //?
 	    .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
 	    .intr_alloc_flags = 0,
-	    .dma_buf_count = 2,
-	    .dma_buf_len = 1024,
+	    .dma_buf_count = I2S_BUF_COUNT,
+	    .dma_buf_len = I2S_BUF_LEN,
 	    .use_apll = false,
 	 };
 	 //install and start i2s driver
@@ -39,11 +45,26 @@ void I2SManager_write_16bitLR(const void *src, size_t size, size_t *bytes_writte
 }
 
 void I2SManager_write_8bitLR(const void *src, size_t size, size_t *bytes_written, TickType_t ticks_to_wait) {
-    uint8_t buff_16bitLR[size*2];
-    for (int i = 0; i < size; i++) {
-        buff_16bitLR[2*i] = 0;
-        buff_16bitLR[2*i+1] = ((uint8_t *) src)[i];
+    uint8_t *buff_16bitLR = malloc(2*CONVERSION_CHUNK_SIZE);
+    *bytes_written = 0;
+
+    if(buff_16bitLR == NULL) {
+    	ESP_LOGE(TAG, "impossible to alloc conversion chunk !");
+    	return;
     }
-    I2SManager_write_16bitLR(buff_16bitLR, size*2, bytes_written, ticks_to_wait);
+
+    for(int offset=0;offset<size;offset+=CONVERSION_CHUNK_SIZE) {
+    	int chunk_size = min(size, CONVERSION_CHUNK_SIZE);
+	    for (int i = 0; i < chunk_size; i++) {
+	        buff_16bitLR[2*i] = 0;
+	        buff_16bitLR[2*i+1] = ((uint8_t *) src)[offset + i];
+	    }
+	   	size_t chunkwritten;
+	    I2SManager_write_16bitLR(buff_16bitLR, chunk_size*2, &chunkwritten, ticks_to_wait);
+	    bytes_written += chunkwritten;
+    }
+
+    free(buff_16bitLR);
+    //Converts to 8bits
     *bytes_written /= 2;
 }
