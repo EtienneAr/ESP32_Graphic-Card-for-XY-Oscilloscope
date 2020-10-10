b18cc1798cd3052008fefa89f4c020f5325b550c
Me <etienne.arlaud@free.fr>
2020-10-10 11:39:34 +0200

    Rect and Arc (no tested)

--- components/DAC_XY_Plotter/priv_include/GraphicObject.h
+++ components/DAC_XY_Plotter/priv_include/GraphicObject.h
@@ -1,17 +1,16 @@
 #ifndef GRAPHICAL_OBJECT_H
 #define GRAPHICAL_OBJECT_H
 
-void   GO_drawPoint(void* start, int x, int y, int intensisty);
-size_t GO_drawPoint_len        ( int x, int y, int intensisty);
+void   GO_drawPoint(void* start, int x, int y, int intensity);
+size_t GO_drawPoint_len        ( int x, int y, int intensity);
 
-void   GO_drawLine(void* start, int x1, int y1, int x2, int y2, int spacing, int intensisty);
-size_t GO_drawLine_len        ( int x1, int y1, int x2, int y2, int spacing, int intensisty);
+void   GO_drawLine(void* start, int x1, int y1, int x2, int y2, int spacing, int intensity);
+size_t GO_drawLine_len        ( int x1, int y1, int x2, int y2, int spacing, int intensity);
 
-/*
-void   GO_drawRect(void* start, int x1, int y1, int x2, int y2, Pen_t pen);
-size_t GO_drawRect_len        ( int x1, int y1, int x2, int y2, Pen_t pen);
 
-void   GO_drawArc(void* start, int x, int y, float r, float a1, float a2, Pen_t pen);
-size_t GO_drawArc_len        ( int x, int y, float r, float a1, float a2, Pen_t pen);
-*/
+void   GO_drawRect(void* start, int x1, int y1, int x2, int y2, int spacing, int intensity);
+size_t GO_drawRect_len        ( int x1, int y1, int x2, int y2, int spacing, int intensity);
+
+void   GO_drawArc(void* start, int x, int y, float r, float a1, float a2, int spacing, int intensity);
+size_t GO_drawArc_len        ( int x, int y, float r, float a1, float a2, int spacing, int intensity);
 #endif
--- components/DAC_XY_Plotter/src/GraphicObject.c
+++ components/DAC_XY_Plotter/src/GraphicObject.c
@@ -4,6 +4,7 @@
 #include <math.h>
 #define max(x,y) ((x)>(y) ? (x) : (y))
 
+
 /*
  * Point
  */
@@ -19,6 +20,7 @@ void GO_drawPoint(void* start, int x, int y, int intensity) {
 		pt_list[i] = (Coord_t) {.x = x, .y = y};
 	}
 }
+
 
 /*
  * Line
@@ -48,4 +50,69 @@ void GO_drawLine(void* start, int x1, int y1, int x2, int y2, int spacing, int i
 			pt_list[n * intensity + i ] = runningPoint;
 		}
 	}
+}
+
+
+/*
+ * Rect
+ */
+
+size_t GO_drawRect_len(int x1, int y1, int x2, int y2, int spacing, int intensity) {
+	size_t res = 0;
+
+	res += GO_drawLine_len(x1, y1, x1, y2, spacing, intensity);
+	res += GO_drawLine_len(x1, y2, x2, y2, spacing, intensity);
+	res += GO_drawLine_len(x2, y2, x2, y1, spacing, intensity);
+	res += GO_drawLine_len(x2, y1, x1, y1, spacing, intensity);
+
+	return res;
+}
+
+
+void GO_drawRect(void* start, int x1, int y1, int x2, int y2, int spacing, int intensity) {
+	void* ptr = start;
+
+	       GO_drawLine(ptr, x1, y1, x1, y2, spacing, intensity);
+	ptr += GO_drawLine_len( x1, y1, x1, y2, spacing, intensity);
+
+           GO_drawLine(ptr, x1, y2, x2, y2, spacing, intensity);
+    ptr += GO_drawLine_len( x1, y2, x2, y2, spacing, intensity);
+
+           GO_drawLine(ptr, x2, y2, x2, y1, spacing, intensity);
+    ptr += GO_drawLine_len( x2, y2, x2, y1, spacing, intensity);
+
+           GO_drawLine(ptr, x2, y1, x1, y1, spacing, intensity);
+    ptr += GO_drawLine_len( x2, y1, x1, y1, spacing, intensity);
+}
+
+/*
+ * Arc
+ */
+
+size_t GO_drawArc_len(int x, int y, float r, float a1, float a2, int spacing, int intensity) {
+    const float length = abs(a1-a2) * r;
+    const int macroPointsNb = length/spacing;
+    const int subPointsNb = macroPointsNb * intensity;
+
+    return subPointsNb * sizeof(Coord_t);
+}
+
+
+void GO_drawArc(void* start, int x, int y, float r, float a1, float a2, int spacing, int intensity) {
+    Coord_t* pt_list = start;
+
+    const float length = abs(a1-a2) * r;
+    const int macroPointsNb = length/spacing;
+    //const int subPointsNb = macroPointsNb * intensity;
+
+    Coord_t runningPoint;
+    float runningAngle;
+    for(int n=0;n<macroPointsNb;n++) {
+        runningAngle = a2 + n * (a1 - a2) / max(macroPointsNb-1, 1);
+        runningPoint.x = x + r * cos(runningAngle);
+        runningPoint.y = x + y * sin(runningAngle);
+        for(int i=0;i<intensity;i++) {
+            pt_list[n * intensity + i ] = runningPoint;
+        }
+    }
 }
