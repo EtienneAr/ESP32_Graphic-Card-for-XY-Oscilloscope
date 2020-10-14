5bc854cc4b18445bcef2329a4af9cbc5527869f5
Me <etienne.arlaud@free.fr>
2020-10-14 21:23:20 +0200

    Demo plane

--- main/app_main.c
+++ main/app_main.c
@@ -13,82 +13,72 @@ static const char* TAG = "Main_app";
 
 #define MIN_MAX_0_255(x) ((x < 0) ? 0 : ((x > 255) ? 255 : x))
 
+Pen_t penStr = {.spacing = 1, .intensity = 1};
+Pen_t penPlane = {.spacing = 1, .intensity = 2};
+Pen_t penCirc = {.spacing = 5, .intensity = 3};
+
+GI_uid_t triangle[3] = {NULL};
+
+typedef struct Circle {
+	int x;
+	int y;
+	int t;
+	GI_uid_t id;
+} Circle_t;
+
+int circle_index = 0;
+Circle_t circles[100] = {NULL};
+
+void drawCircle(float t) {
+	for(int i=0;i<circle_index;i++) {
+		GI_uid_t temp = XYPlotter_drawArc(circles[i].x, circles[i].y, t - circles[i].t, 0, 2*3.141592, penCirc);
+		XYPlotter_delete(circles[i].id);
+		circles[i].id = temp;
+	}
+}
+
 esp_err_t app_main()
 {
     XYPlotter_init(1000000);
     esp_log_level_set("I2S", ESP_LOG_INFO);
 
-    Pen_t pen = {.spacing = 1, .intensity = 2};
-    Pen_t pen_circ = {.spacing = 1, .intensity = 1};
+    XYPlotter_drawString("Le choc", 40, 30, 28, penStr);
+    XYPlotter_drawString("supersonique", 10, 0, 28, penStr);
+    //XYPlotter_drawArc(127, 127, 127, 0, 2*3.141592, pen_circ);
+	//XYPlotter_drawLine(0  , y_bar_L-10, 0  , y_bar_L+10, penLine);
+	triangle[0] = XYPlotter_drawLine(0, 155, 0, 145, penPlane);
+	triangle[1] = XYPlotter_drawLine(0, 145, 10, 150, penPlane);
+	triangle[2] = XYPlotter_drawLine(10, 150, 0, 155, penPlane);
 
-    /*
-	XYPlotter_drawLine(0, 0, 255, 255, pen);
-	XYPlotter_drawLine(255, 0, 0, 0, pen);
-	XYPlotter_drawLine(255, 255, 255, 0, pen);
-	XYPlotter_drawLine(0, 255, 255, 255, pen);
-    XYPlotter_drawLine(0, 0, 0, 255, pen);
-	*/
-
-    //XYPlotter_drawLine(50, 150, 50, 50, pen);
-    XYPlotter_drawArc(127, 127, 127, 0, 2*3.141592, pen_circ);
-    XYPlotter_drawArc(127, 127, 110, 0.3, 3.141592-0.3, pen_circ);
-    XYPlotter_drawArc(127, 127, 140, 0, 3.141592, pen_circ);
-    //XYPlotter_drawRect(50, 50, 150, 150, pen);
-    
-    XYPlotter_drawString("Bonjour, monde !", 10, 127, 21, pen);
-    XYPlotter_drawString("Et CC Tristan   ;P ", 40, 70, 21, pen);
-
-	//XYPlotter_drawString("r s t u v|w x y z.,:;!?\\/$ % +-^=({[)}]", 0, 255, 28, pen);
-	
-	
-
-	float x_ball = 127;
-	float y_ball = 127;
-	float vx_ball = 1;
-	float vy_ball = 0.7;
-
-	float y_bar_L = 127;
-	float y_bar_R = 127;
-
-	vTaskDelay(1000);
+    int x_plane = 5;
+    int y_plane = 155;
+    float t = 0;
 	while(1) {
-		Pen_t penLine = {.spacing = 1, .intensity = 5};
-		Pen_t penPoint = {.spacing = 1, .intensity = 25};
-
-		XYPlotter_clearScreen();
-		XYPlotter_drawLine(0  , y_bar_L-10, 0  , y_bar_L+10, penLine);
-		XYPlotter_drawPoint(x_ball, y_ball, penPoint);
-		XYPlotter_drawLine(255, y_bar_R-10, 255, y_bar_R+10, penLine);
-
-
-		if(vx_ball > 0) {
-			y_bar_L += ((y_bar_L < y_ball) ? 1 : -1) * -0.33;
-			y_bar_R += ((y_bar_R < y_ball) ? 1 : -1) * 1.0;
-		} else {
-			y_bar_L += ((y_bar_L < y_ball) ? 1 : -1) * 1.0;
-			y_bar_R += ((y_bar_R < y_ball) ? 1 : -1) * -0.33;
+		if(x_plane < 250) {
+			XYPlotter_move(triangle[0], 1, 0);
+			XYPlotter_move(triangle[1], 1, 0);
+			XYPlotter_move(triangle[2], 1, 0);
+		} else if(x_plane == 250) {
+			XYPlotter_delete(triangle[0]);
+			XYPlotter_delete(triangle[1]);
+			XYPlotter_delete(triangle[2]);
 		}
 
-		y_bar_R = fmax(fmin(y_bar_R, 255-10), 0+10);
-		y_bar_L = fmax(fmin(y_bar_L, 255-10), 0+10);
-
-		x_ball += vx_ball * 1.0;
-		y_ball += vy_ball * 1.0;
-
-		if( (y_ball <= 0 && vy_ball < 0) || (y_ball >= 255 && vy_ball> 0)) {
-			vy_ball *= -1;
-			ESP_LOGW(TAG, "Hit one");
-		}
-		if( (x_ball <= 0 && vx_ball < 0) || (x_ball >= 255 && vx_ball > 0)) {
-			vx_ball *= -1;
-			ESP_LOGW(TAG, "Hit two");
+		if(x_plane%1 == 0) {
+			drawCircle(t);
 		}
 
-		x_ball = fmax(fmin(x_ball, 255), 0);
-		y_ball = fmax(fmin(y_ball, 255), 0);
+		vTaskDelay(50);
+		x_plane += 1;
+		t = x_plane / 1.4;
 
-		vTaskDelay(10);
-		//ESP_LOGW(TAG, "Free heap memory : %d", xPortGetFreeHeapSize());
+		if(x_plane % 32 == 0 && x_plane < 300) {
+			circles[circle_index].x = x_plane;
+			circles[circle_index].y = y_plane;
+			circles[circle_index].t = t;
+			circles[circle_index].id = NULL;
+			circle_index++;
+		}
 	}
 
     return ESP_OK;
