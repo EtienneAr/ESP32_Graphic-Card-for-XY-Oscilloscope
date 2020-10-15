841ac02048b52b7d168387c7d9a6d63ffda878f2
Me <etienne.arlaud@free.fr>
2020-10-15 19:05:07 +0200

    WiP: move everything in component

--- Makefile
+++ Makefile
@@ -6,15 +6,3 @@
 PROJECT_NAME := DAC_XY_Plotter
 
 include $(IDF_PATH)/make/project.mk
-
-.DEFAULT_GOAL := myAll
-
-myAll: generate-font all
-
-FONT_CODE_C_FILENAME_PREFIX := components/DAC_XY_Plotter/src/GraphicObject_char
-
-generate-font: $(FONT_CODE_C_FILENAME_PREFIX)_thin.c $(FONT_CODE_C_FILENAME_PREFIX)_wide.c
-
-$(FONT_CODE_C_FILENAME_PREFIX)_%.c: font_%.json
-	@echo "Generating code for font $*"
-	python3 font_to_code.py $< > components/DAC_XY_Plotter/src/GraphicObject_char_$*.c
--- components/DAC_XY_Plotter/component.mk
+++ components/DAC_XY_Plotter/component.mk
@@ -3,3 +3,18 @@ COMPONENT_ADD_INCLUDEDIRS := ./include
 COMPONENT_PRIV_INCLUDEDIRS +=  ./priv_include
 
 COMPONENT_SRCDIRS += ./src
+
+
+FONT_CODE_C_FILENAME_PREFIX := $(COMPONENT_PATH)/src/GraphicObject_char
+
+COMPONENT_EXTRA_CLEAN := $(FONT_CODE_C_FILENAME_PREFIX)*
+
+$(COMPONENT_LIBRARY): generate_files
+
+.PHONY: generate_files
+
+generate_files: $(FONT_CODE_C_FILENAME_PREFIX)_thin.c $(FONT_CODE_C_FILENAME_PREFIX)_wide.c
+
+$(FONT_CODE_C_FILENAME_PREFIX)_%.c: $(COMPONENT_PATH)/data/font_%.json
+	@echo "Generating code for font $*"
+	python3 $(COMPONENT_PATH)/font_to_code.py $< > $@
